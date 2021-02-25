16b747ad5091af532c030d652732ccb95c87c8ef
talisma <62837677+talisma-cassoma@users.noreply.github.com>
2021-02-25 22:29:42 +0100

    change the database mysql to sqlite

--- App/modules/db.js
+++ App/modules/db.js
@@ -1,10 +1,19 @@
 //conec data base
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  // o parametro `host`  é somente requirido para outras databases, 
+  //o sqlite é uma banco de dados que nao que fica armazenado na tua maquina 
+  //e nao é servida pelo servidor como as outras bases de dados 
+  dialect: 'sqlite',
+  storage: 'sqliteDB/database.sqlite' //vai criar essa rota para a dB
+});
 
+/*//na minha maquina(pc) usei uma base de dados msql:
 const sequelize = new Sequelize('postApp', 'root', 'password', {
     host: "localhost", //o sitio onde está hospedado(no meu pc ) 
     dialect: "mysql" //o tipo de DB 
 })
+*/ 
 //exportar as duas variaveis em forma de obj
 module.exports = {
     Sequelize: Sequelize,
